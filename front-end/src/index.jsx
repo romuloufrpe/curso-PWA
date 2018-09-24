3401cd34363fe5468faab0f9b9f361bedbdb7214
romuloufrpe <romulo.ferreira.ufrpe@gmail.com>
2018-09-23 21:36:18 -0300

    curso de desenvolvimento PWA

---
+++ front-end/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './img/favicon.ico';
+import './css/index.css';
+import './css/pure-min.css';
+
+ReactDOM.render(
+    <h1>Bem Vindo ao React!</h1>,
+    document.querySelector("#main")
+)
