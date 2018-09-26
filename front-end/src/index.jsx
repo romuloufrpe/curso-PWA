38d6c4650892b87f02d1c6ffc9d5ca2d47fdb77d
romuloufrpe <romulo.ferreira.ufrpe@gmail.com>
2018-09-26 12:10:24 -0300

    components adicionado

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom'
 import './img/favicon.ico';
 import './css/index.css';
 import './css/pure-min.css';
+import App from './components/App.jsx';
 
 ReactDOM.render(
-    <h1>Bem Vindo ao React!</h1>,
+    < App/>,
     document.querySelector("#main")
 )
